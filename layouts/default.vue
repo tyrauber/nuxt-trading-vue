ce87f395a43886d48b616f1192ef4a9b534f005b
Ty Rauber <tyrauber@mac.com>
2019-12-27 17:56:12 +0530

    Nuxt trading-vue example

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <section class="main-content">
+      <div class="container column is-10">
+        <nuxt />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+</script>
